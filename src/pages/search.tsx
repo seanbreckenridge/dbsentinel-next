77d5c358bcd51493fe32769289bac23695cf0f98
Sean Breckenridge <seanbrecke@gmail.com>
2023-01-18 23:23:06 -0800

    improvements for mobile

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -39,13 +39,13 @@ const unslugify = (slug: string) => {
 
 const statusLabel = (status: string) => {
   if (status === "unapproved") {
-    return "Added on ";
+    return "Added on: ";
   } else if (status === "approved") {
-    return "Added on ";
+    return "Added on: ";
   } else if (status === "denied") {
-    return "Denied on ";
+    return "Denied on: ";
   } else if (status === "deleted") {
-    return "Deleted on ";
+    return "Deleted on: ";
   }
 };
 
@@ -285,7 +285,7 @@ const Query: NextPage = () => {
                 }}
               />
             </label>
-            <div className="m-1 flex flex-row items-center justify-start">
+            <div className="m-1 flex flex-col items-left justify-center sm:flex-row">
               <label htmlFor="entryType" className="m-1">
                 Type
                 <select
@@ -359,7 +359,7 @@ const Query: NextPage = () => {
                 </select>
               </label>
             </div>
-            <div className="m-1 flex flex-row items-center justify-start">
+            <div className="m-1 flex flex-col items-left justify-center sm:flex-row">
               <label htmlFor="orderBy" className="m-1">
                 Order By
                 <select
@@ -494,7 +494,7 @@ const Query: NextPage = () => {
               </div>
             ) : query.data && query.data.total_count > 0 ? (
               <div className="container mx-auto flex w-full flex-col items-center justify-center">
-                <table className="table-fixed">
+                <table className="table-fixed max-w-full">
                   <thead>
                     <tr className="border-2 border-black bg-gray-100">
                       <th className="px-2 py-2"></th>
@@ -504,7 +504,7 @@ const Query: NextPage = () => {
                         <div>Dates</div>
                         <FontAwesomeIcon
                           icon={faQuestionCircle}
-                          className="mx-1 mr-2 h-5 w-5"
+                          className="mx-1 mr-2 h-5 w-5 cursor-pointer"
                           title={DELETED_INFO}
                           onClick={() => alert(DELETED_INFO)}
                         />
@@ -581,7 +581,7 @@ const Query: NextPage = () => {
                             <div className="flex w-full flex-col items-center justify-center">
                               <div>{entry.title}</div>
                               <hr className="my-2 w-10/12" />
-                              <ul className="mb-2 flex w-full flex-row items-center justify-center">
+                              <ul className="mb-2 flex w-full flex-col sm:flex-row items-center justify-center">
                                 <MetadataRow
                                   keyName="media type"
                                   value={
@@ -609,7 +609,7 @@ const Query: NextPage = () => {
                                   }
                                 />
                               </ul>
-                              <ul className="flex w-full flex-row items-center justify-center">
+                              <ul className="flex w-full sm:flex-row flex-col items-center justify-center">
                                 {/* eslint-disable-next-line @typescript-eslint/no-unsafe-argument */}
                                 {Object.keys(entry.json_data)
                                   .filter((key: string) =>
@@ -647,7 +647,7 @@ const Query: NextPage = () => {
                               </ul>
                             </div>
                           </td>
-                          <td className="w-2/12 border px-2 py-2">
+                          <td className="w-2/12 border p-0 sm:p-1 md:p-2">
                             <div className="flex w-full flex-col items-center justify-center">
                               <div className="text-xs">
                                 {"Start Date: "}
